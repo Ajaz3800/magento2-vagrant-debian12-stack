b70e8dea0c984b7e392a4755f4e35a81175f1d1b
Ajaz3800 <shaikhajaz38000@gmail.com>
2026-02-15 09:43:21 +0530

    mysql.sh fixed

--- lib/MySQL.sh
+++ lib/MySQL.sh
@@ -15,8 +15,8 @@ install_mysql() {
         export DEBIAN_FRONTEND=noninteractive
         dpkg -i "$mysql_repo_pkg" >/dev/null 2>&1
 
-        run_with_spinner "Updating package list" retry 3 apt-get update || return 1
-        run_with_spinner "Installing MySQL server" retry 3 apt-get install -y mysql-server || return 1
+        retry 3 apt-get update || return 1
+        retry 3 apt-get install -y mysql-server || return 1
 
         success "MySQL 8 installed successfully"
     fi
