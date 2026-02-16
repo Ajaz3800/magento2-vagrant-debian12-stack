940b7bc7beed636de7ddc5055fdf8118df3bfcc0
Ajaz3800 <shaikhajaz38000@gmail.com>
2026-02-16 17:45:36 +0530

    file names changed, and updated the code.

--- Nginx/nginx.sh
+++ Nginx/nginx.sh
@@ -44,10 +44,10 @@ install_nginx() {
     fi
 
 
-    # Copy Magento 2 config
-    local magento_sample="./Nginx/config/magento2.conf.sample"
-    local magento_dest="/etc/nginx/sites-available/magento2.conf"
-    local magento_enabled="/etc/nginx/sites-enabled/magento2.conf"
+    # Copy Magento 2 config-back
+    local magento_sample="./Nginx/config/magento2-back.conf.sample"
+    local magento_dest="/etc/nginx/sites-available/magento2-back.conf"
+    local magento_enabled="/etc/nginx/sites-enabled/magento2-back.conf"
 
     if [[ ! -f "$magento_sample" ]]; then
         error "Magento sample config not found at $magento_sample"
@@ -62,16 +62,46 @@ install_nginx() {
     run_step "Updating PHP-FPM version in config" \
         sed -i "s|php[0-9.]*-fpm.sock|php${PHP_VER}-fpm.sock|g" "$magento_dest" || return 1
 
-    # Update server_name
-    run_step "Updating domain in config" \
-        sed -i "s|server_name .*;|server_name ${BASE_URL};|g" "$magento_dest" || return 1
+    # # Update server_name
+    # run_step "Updating domain in config" \
+    #     sed -i "s|server_name .*;|server_name ${BASE_URL};|g" "$magento_dest" || return 1
 
     # Enable Magento site
     if [[ ! -L "$magento_enabled" ]]; then
         run_step "Enabling Magento site" \
             ln -s "$magento_dest" "$magento_enabled" || return 1
     else
-        success "Magento site already enabled"
+        success "Magento site already enabled for varnish"
+    fi
+
+    # Copy Magento 2 config-front
+    local magento_sample_front="./Nginx/config/magento2-front.conf.sample"
+    local magento_dest_front="/etc/nginx/sites-available/magento2-front.conf"
+    local magento_enabled_front="/etc/nginx/sites-enabled/magento2-front.conf"
+
+    if [[ ! -f "$magento_sample" ]]; then
+        error "Magento sample config not found at $magento_sample"
+        return 1
+    fi
+
+    run_step "Copying Magento Nginx config" \
+        cp "$magento_sample" "$magento_dest" || return 1
+    
+
+    # Update PHP-FPM socket
+    run_step "Updating PHP-FPM version in config" \
+        sed -i "s|php[0-9.]*-fpm.sock|php${PHP_VER}-fpm.sock|g" "$magento_dest_front" || return 1
+
+    # # Update server_name
+    # run_step "Updating domain in config" \
+    #     sed -i "s|server_name .*;|server_name ${BASE_URL};|g" "$magento_dest_front" || return 1
+
+    # Enable Magento site
+    if [[ ! -L "$magento_enabled_front" ]]; then
+        run_step "Enabling Magento site" \
+            ln -s "$magento_dest_front" "$magento_enabled_front" || return 1
+    else
+        success "Magento site already enabled for varnish"
     fi
 
 
